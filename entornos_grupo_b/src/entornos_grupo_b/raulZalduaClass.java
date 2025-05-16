9c85bbb6fe573baa069102bc77f15860f16bdbe7
R1ul09 <128465165+R1ul09@users.noreply.github.com>
2025-05-16 14:11:06 +0200

    CAMBIO DE RAUL NUEVA CLASS

---
+++ entornos_grupo_b/src/entornos_grupo_b/raulZalduaClass.java
@@ -0,0 +1,10 @@
+package entornos_grupo_b;
+
+public class raulZalduaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HOLA JOSE LUIS, ESTA ES MI CLASE");
+	}
+
+}
