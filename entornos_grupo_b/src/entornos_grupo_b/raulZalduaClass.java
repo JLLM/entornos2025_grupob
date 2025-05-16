53884bd5b91461fe8164c6e8deb4d87c644c44fc
JLLM <jllm9001@gmail.com>
2025-05-16 14:52:47 +0200

    CAMBIO SUMO Y RESTO

--- entornos_grupo_b/src/entornos_grupo_b/raulZalduaClass.java
+++ entornos_grupo_b/src/entornos_grupo_b/raulZalduaClass.java
@@ -4,7 +4,7 @@ public class raulZalduaClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HOLA JOSE LUIS, ESTA ES MI CLASE");
+		System.out.println("HOLA JOSE LUIS, ESTA ES MI ()");
 		System.out.println("YO SIGO A MIS COSAS CON MIS CAMBIOS");
 		System.out.println("YO SIGO A MIS COSAS CON MIS CAMBIOS JOSE LUIS");
 	}
