05fb914635c997da8be1d2571a1f8154e7d14952
hector <hectorrf.correo@gmail.com>
2025-05-19 07:15:00 +0200

    CAMBIOS ALVARO

--- entornos_grupo_b/src/entornos_grupo_b/AlvaroRodriguez.java
+++ entornos_grupo_b/src/entornos_grupo_b/AlvaroRodriguez.java
@@ -4,7 +4,8 @@ public class AlvaroRodriguez {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Odio LM");
+		System.out.println("Odio LM");
 	}
 
 }
