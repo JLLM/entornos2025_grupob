a31f093241bc25f889d0396981b185d19ad23787
Xentauross <vicentepeinadoalcedo@gmail.com>
2025-05-19 06:56:08 +0200

    Ole ole

--- entornos_grupo_b/src/entornos_grupo_b/vpaPrueba.java
+++ entornos_grupo_b/src/entornos_grupo_b/vpaPrueba.java
@@ -5,8 +5,10 @@ public class vpaPrueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Hola caracola
-		System.out.print("Version 1.0.2");
-		System.out.print("Version 1.0.3");
+		System.out.print("Version de vicente");
+		System.out.print("Version 2.0");
+		System.out.println("Esto ya funciona");
+		System.out.println("Ole ole");
 	}
 
 }
