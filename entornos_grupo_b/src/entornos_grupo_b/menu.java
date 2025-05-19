9fc883c71e9fb1f2cb44034463264763fd3db5eb
Xentauross <vicentepeinadoalcedo@gmail.com>
2025-05-19 07:01:54 +0200

    Merge branch 'master' of https://github.com/JLLM/entornos2025_grupob.git
    Adios

--- entornos_grupo_b/src/entornos_grupo_b/menu.java
+++ entornos_grupo_b/src/entornos_grupo_b/menu.java
@@ -5,8 +5,10 @@ public class menu {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Hola caracola
+		// Adios carabola
 		System.out.print("Version 1.0.2");
 		System.out.print("Version 1.0.3");
+
 	}
 
 }
